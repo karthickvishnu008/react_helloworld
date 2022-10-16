8b4e276d0176877d62143ac49df9371d8aef7081
karthickvishnu008 <84023498+karthickvishnu008@users.noreply.github.com>
2022-10-16 18:13:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="center">
+    <p><center><h1>Hello world!</h1></center></p>
+    </div>
+  );
+}
+
+export default App;
